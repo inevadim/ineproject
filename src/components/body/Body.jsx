20d0a7137bd9d168170ecd50d5a4bf22d336dc1f
inevadim <78503414+inevadim@users.noreply.github.com>
2022-12-06 01:16:21 +0300

    delete comments

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -7,9 +7,6 @@ export const Body = () => {
       {pizzasJson.map((obj, i) => {
         return <Card {...obj} key={i} />;
       })}
-      {/* <Card title="Card 1" alt_title="Описание 1" price="127" srcPicca={pizzaMooshrome} />
-      <Card title="Card 2" alt_title="Описание 2" price="128" srcPicca={pizzaVillage} />
-      <Card title="Card 3" alt_title="Описание 3" price="129" srcPicca={pizzaVillage} /> */}
     </div>
   );
 };

c0a1a0962085c0fd8f92307961f13916b514abaf
inevadim <78503414+inevadim@users.noreply.github.com>
2022-12-12 16:17:41 +0300

    refactoring Popup sort

--- src/components/header/menu/popupSort/PopupSort.jsx
+++ src/components/header/menu/popupSort/PopupSort.jsx
@@ -1,13 +1,19 @@
 import styles from './PopupSort.module.scss';
 
 export const PopupSort = () => {
+  const articleSort = ['популярности', 'цене', 'алфавиту'];
   return (
     <div className={styles.popupSort}>
       <ul>
+        {articleSort.map(value => {
+          return <li key={value}>{value}</li>;
+        })}
+      </ul>
+      {/* <ul>
         <li>популярности</li>
         <li>цене</li>
         <li>алфавиту</li>
-      </ul>
+      </ul> */}
     </div>
   );
 };

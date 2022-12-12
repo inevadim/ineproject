2ea0adc5dd25471286d7e5591269f24fdcdee70a
inevadim <78503414+inevadim@users.noreply.github.com>
2022-12-12 23:48:12 +0300

    add new folder and files contacts

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Header } from './components/header/Header';
 import { Body } from './components/body/Body';
 import { Footer } from './components/footer/Footer';
 import styles from './App.module.scss';
+import { Contacts } from './components/body/contacts/Contacts';
 
 function App() {
   return (
     <div className={styles.app}>
       <Header />
       <Body />
+      {/* <Contacts /> */}
       <Footer />
     </div>
   );
---
+++ src/components/body/contacts/Contacts.jsx
@@ -0,0 +1,5 @@
+import styles from './Contacts.module.scss';
+
+export const Contacts = () => {
+  return <div className={styles.contacts}>Contacts</div>;
+};

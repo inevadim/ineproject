3af2b6c1a95e7675dd1d4ca6f724bcd85c97c508
inevadim <78503414+inevadim@users.noreply.github.com>
2022-12-12 22:23:55 +0300

    add and commenting skeleton for item

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,8 +1,9 @@
 import styles from './Body.module.scss';
 import { Ticket } from './ticket/Ticket';
-// import pizzasJson from '../../assets/bd/pizzas.json';
 import { useState, useEffect } from 'react';
 import { BigTicket } from './ticket/bigTicket/BigTicket';
+// import Skeleton from '@mui/material/Skeleton';
+// import Stack from '@mui/material/Stack';
 
 export const Body = () => {
   //https://63973a0b86d04c76338f0a50.mockapi.io/items
@@ -21,6 +22,11 @@ export const Body = () => {
   return (
     <div className={styles.wrapperBody}>
       <div className={styles.body}>
+        {/* <Stack spacing={1}>
+          <Skeleton variant="circular" width={190} height={190} />
+          <Skeleton variant="rectangular" width={190} height={80} />
+          <Skeleton variant="rounded" width={190} height={80} />
+        </Stack> */}
         {items.map((obj, i) => {
           return <Ticket {...obj} key={i} />;
         })}

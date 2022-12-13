8c957191422f879a1701c94f36f61cfeb92f8a4e
inevadim <78503414+inevadim@users.noreply.github.com>
2022-12-13 20:21:00 +0300

    delete redux in component contacts

--- src/components/body/contacts/Contacts.jsx
+++ src/components/body/contacts/Contacts.jsx
@@ -1,21 +1,9 @@
 import styles from './Contacts.module.scss';
-import { useSelector, useDispatch } from 'react-redux';
-import { decrement, increment } from '../../../redux/slices/filterSlice';
-export const Contacts = () => {
-  const count = useSelector(state => state.filterSlice.value);
-  const dispatch = useDispatch();
 
+export const Contacts = () => {
   return (
     <div className={styles.contacts}>
-      <div>
-        <button aria-label="Increment value" onClick={() => dispatch(increment())}>
-          Increment
-        </button>
-        <span>{count}</span>
-        <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
-          Decrement
-        </button>
-      </div>
+      <div>Contacts</div>
     </div>
   );
 };
